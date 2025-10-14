dbae3d7115f8e4d0efa4798461c79f350c42b6fb
Løwe <mathildeloewe@gmail.com>
2025-10-14 19:56:32 +0200

    remove: extra code

--- public/scripts/topButton.js
+++ public/scripts/topButton.js
@@ -1,18 +1,3 @@
-// window.addEventListener("scroll", () => {
-//   const button = document.getElementById("scrollButton");
-//   if (!button) return;
-
-//   const scrollY = window.scrollY;
-
-//   if (scrollY < 850 || scrollY > 3500) {
-//     // Hide if before 300px or after 1000px
-//     button.classList.add("opacity-0", "pointer-events-none");
-//   } else {
-//     // Show only between 300px and 1000px
-//     button.classList.remove("opacity-0", "pointer-events-none");
-//   }
-// });
-
 function toggleButtonVisibility() {
   const button = document.getElementById("scrollButton");
   if (!button) return;
@@ -27,4 +12,4 @@ function toggleButtonVisibility() {
 }
 
 window.addEventListener("scroll", toggleButtonVisibility);
-window.addEventListener("load", toggleButtonVisibility); // 👈 Run once on load
+window.addEventListener("load", toggleButtonVisibility);
